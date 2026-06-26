06293182f4997736ddfbd145e6d24fff978c635c
Quebeta-Technologies <161155622+Quebeta-Technologies@users.noreply.github.com>
2026-06-26 19:46:10 +0530

    54564646

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -24,7 +24,7 @@ export default function About() {
             <div className="welcome-img-wrap">
               <div className="welcome-img">
                 <img
-                  src="/public/images/about.png"
+                  src="/images/about.png"
                   alt="Sri Krishna PG furnished room in Baner Pune"
                 />
               </div>
